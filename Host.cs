434425f3d575d53502cf6ca7ab6186af23256379
muntashir <kazimuntashir@gmail.com>
2014-03-10 21:34:51 -0400

    Initial commit
    Only ping itself works for now.

---
+++ Host.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pingo
+{
+    public class Host
+    {
+        protected string hostname;
+        Ping pingo;
+        PingReply pingoReply;
+        bool online;
+
+        public Host(string hostname)
+        {
+            this.hostname = hostname;
+            pingo = new Ping();           
+
+            Ping();
+        }
+
+        public void Ping()
+        {
+            online = false;
+
+            try
+            {
+                pingoReply = pingo.Send(hostname);
+                online = pingoReply.Status == IPStatus.Success;
+            }
+            catch (PingException)
+            {
+            }
+        }
+
+        public bool IsOnline()
+        {
+            return online;
+        }
+
+        public new String[] ToString()
+        {
+            String[] array = {hostname, online ? "Online" : "Offline"};
+
+            return array;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pingo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Host> hosts;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            hosts = new List<Host>();
+        }
+
+        private void btnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (txtInput.Text == "" || txtInput.Text == "Enter a hostname or IP")
+                    throw new Exception("Invalid Input");
+                else
+                {
+                    hosts.Add(new Host(txtInput.Text));
+
+                    if (hosts[hosts.Count - 1].IsOnline() == true)
+                        MessageBox.Show("y");
+                    else
+                        MessageBox.Show("n");
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
