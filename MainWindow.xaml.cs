1b60e514df06d63035f2e920facfc3821e0b2617
muntashir <kazimuntashir@gmail.com>
2014-03-11 00:19:05 -0400

    Hosts are now displayed in ListBox

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Pingo
 {
@@ -22,12 +23,25 @@ namespace Pingo
     public partial class MainWindow : Window
     {
         List<Host> hosts;
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
 
         public MainWindow()
         {
             InitializeComponent();
 
             hosts = new List<Host>();
+
+            txtInput.Focus();
+            txtInput.SelectAll();
+
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0,10,0);
+            dispatcherTimer.Start();
+        }
+
+        private void dispatcherTimer_Tick(object sender, object e)
+        {
+            Refresh();
         }
 
         private void btnEnter_Click(object sender, RoutedEventArgs e)
@@ -39,18 +53,55 @@ namespace Pingo
                 else
                 {
                     hosts.Add(new Host(txtInput.Text));
-
-                    if (hosts[hosts.Count - 1].IsOnline() == true)
-                        MessageBox.Show("y");
-                    else
-                        MessageBox.Show("n");
+                    UpdateOutput();
                 }
-
             }
             catch(Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
+
+            txtInput.Focus();
+            txtInput.SelectAll();
+        }
+
+        private void UpdateOutput()
+        {
+            lsBox.Items.Clear();
+
+            foreach (Host host in hosts)
+            {
+                lsBox.Items.Add(host.ToString()[0] + "\t" + host.ToString()[1]);
+            }
+        }
+
+        private void Refresh()
+        {
+            foreach (Host host in hosts)
+            {
+                host.Ping();
+            }
+
+            UpdateOutput();
+        }
+
+        private void ScrollBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+
+        private void btnRefreshAll_Click(object sender, RoutedEventArgs e)
+        {
+            Refresh();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int minutes = int.Parse(txtInterval.Text);
+
+            dispatcherTimer.Stop();
+            dispatcherTimer.Interval = new TimeSpan(0, minutes, 0);
+            dispatcherTimer.Start();
         }
     }
 }
